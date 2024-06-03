a4953ad76384712563cd578f3df8c42d314c1700
Nihaal Shaikh <nihaal.shaikh@ldesignmedia.nl>
2024-06-03 11:32:55 +0200

    #86bygp4h5 one time and daily scripts complete

---
+++ classes/task/daily_task.php
@@ -0,0 +1,80 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * A scheduled task for CAS user sync.
+ *
+ * @package    local_greetings
+ * @copyright  2015 Vadim Dvorovenko <Vadimon@mail.ru>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace local_greetings\task;
+
+use dml_exception;
+
+/**
+ * A daily task class to delete accounts which are inactive for more than 3 years excluding '1970-01-01'.
+ *
+ * @copyright  2015 Vadim Dvorovenko <Vadimon@mail.ru>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class daily_task extends \core\task\scheduled_task {
+
+    /**
+     * Get a descriptive name for this task (shown to admins).
+     *
+     * @return string
+     */
+    public function get_name(): string {
+        return 'Daily task';
+    }
+
+    /**
+     * Run daily task.
+     */
+    public function execute(): string {
+        global $DB, $CFG;
+
+        try {
+            // Define the date three years ago.
+            $threeyearsago = strtotime('-3 years', strtotime('now'));
+            // Find and delete accounts that have not been active for more than 3 years, excluding '1970-01-01'.
+            $sql = "SELECT id FROM {user} WHERE lastlogin != 0 AND lastlogin < :threeyearsago AND deleted != 1";
+            $accountstodelete = $DB->get_records_sql($sql, ['threeyearsago' => $threeyearsago]);
+
+            if (!empty($accountstodelete)) {
+                require_once($CFG->dirroot . '/user/lib.php');
+
+                foreach ($accountstodelete as $account) {
+                    $user = $DB->get_record('user', ['id' => $account->id]);
+                    if ($user) {
+                        // Delete the user using Moodle's built-in function, so that it is seen in the report.
+                        delete_user($user);
+                    }
+                }
+
+                return "Deleted " . count($accountstodelete) . " accounts.\n";
+            } else {
+                return "No accounts to delete.\n";
+            }
+
+        } catch (dml_exception $e) {
+            return "Error: " . $e->getMessage();
+        }
+    }
+
+}
---
+++ classes/task/one_time_task.php
@@ -0,0 +1,80 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * A scheduled task for CAS user sync.
+ *
+ * @package    local_greetings
+ * @copyright  2015 Vadim Dvorovenko <Vadimon@mail.ru>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+namespace local_greetings\task;
+
+use dml_exception;
+
+/**
+ * A one time task class to delete accounts which are inactive and older than 6 months.
+ *
+ * @copyright  2015 Vadim Dvorovenko <Vadimon@mail.ru>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class one_time_task extends \core\task\scheduled_task {
+
+    /**
+     * Get a descriptive name for this task (shown to admins).
+     *
+     * @return string
+     */
+    public function get_name(): string {
+        return 'One time task';
+    }
+
+    /**
+     * Run one time task.
+     */
+    public function execute(): string {
+        global $DB, $CFG;
+
+        try {
+            // Define the date six months ago in Unix timestamp format.
+            $sixmonthsago = time() - (6 * 30 * 24 * 60 * 60); // Approximation of six months.
+
+            // Find and delete accounts that have never been active and were created more than 6 months ago.
+            $sql = "SELECT id FROM {user} WHERE lastlogin = 0 AND timecreated < :sixmonthsago AND id != 1 AND deleted != 1";
+            $accountstodelete = $DB->get_records_sql($sql, ['sixmonthsago' => $sixmonthsago]);
+
+            if (!empty($accountstodelete)) {
+                require_once($CFG->dirroot . '/user/lib.php');
+
+                foreach ($accountstodelete as $account) {
+                    $user = $DB->get_record('user', ['id' => $account->id]);
+                    if ($user) {
+                        // Delete the user using Moodle's built-in function, so that it is seen in the report.
+                        delete_user($user);
+                    }
+                }
+
+                return "Deleted " . count($accountstodelete) . " accounts.\n";
+            } else {
+                return "No accounts to delete.\n";
+            }
+
+        } catch (dml_exception $e) {
+            return "Error: " . $e->getMessage();
+        }
+    }
+
+}
